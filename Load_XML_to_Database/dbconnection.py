d0be07ab4cb4f49e61ecaccdd276c765dbc32e12
Heli_Patel <helipatel2791@gmail.com>
2020-05-04 17:47:43 -0400

    3

--- Load_XML_to_Database/dbconnection.py
+++ Load_XML_to_Database/dbconnection.py
@@ -1,6 +1,6 @@
 import pymysql
 
 def getcursorconnection():
-    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='227pankaj27', db='usa_cars')
+    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='yourpassword', db='usa_cars')
     cursor = conn.cursor()
     return conn, cursor
