22ff06835836b56a7c9a8a8dc0844e51fe74711f
Heli_Patel <helipatel2791@gmail.com>
2020-05-06 15:35:02 -0400

    4

--- CSV_file_logs/dbconnection.py
+++ CSV_file_logs/dbconnection.py
@@ -1,6 +1,6 @@
 import pymysql
 
 def getcursorconnection():
-    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='227pankaj27', db='filesrecord')
+    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='passwd', db='filesrecord')
     cursor = conn.cursor()
     return conn, cursor
